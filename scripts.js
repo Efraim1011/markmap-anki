09b47b71a97c827fb310b61650767ea2657d1a16
EFRAIM ALMEIDA <efraimalmeida100@gmail.com>
2024-07-23 10:22:47 -0300

    Update scripts.js
    git add index.html scripts.js
    git commit -m "Adiciona verificações e correções no scripts.js e index.html"
    git push origin main

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+console.log("Script carregado com sucesso!");
+
 function toggleVisibility(id) {
     var element = document.getElementById(id);
-    if (element.style.display === "none") {
+    if (element.style.display === "none" || element.style.display === "") {
         element.style.display = "block";
     } else {
         element.style.display = "none";
@@ -8,25 +10,33 @@ function toggleVisibility(id) {
 }
 
 async function fetchFiles(folder, elementId) {
-    const response = await fetch(`https://api.github.com/repos/Efraim1011/markmap-anki/contents/${folder}`);
-    const files = await response.json();
-
-    const list = document.getElementById(elementId);
-    list.innerHTML = ''; // Clear existing list items
-
-    files.forEach(file => {
-        if (file.name.endsWith('.html')) {
-            const listItem = document.createElement('li');
-            const link = document.createElement('a');
-            link.href = `${folder}/${file.name}`;
-            link.textContent = file.name.replace(/%20/g, ' ');
-            listItem.appendChild(link);
-            list.appendChild(listItem);
+    try {
+        const response = await fetch(`https://api.github.com/repos/Efraim1011/markmap-anki/contents/${folder}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-    });
+        const files = await response.json();
+
+        const list = document.getElementById(elementId);
+        list.innerHTML = ''; // Clear existing list items
+
+        files.forEach(file => {
+            if (file.name.endsWith('.html')) {
+                const listItem = document.createElement('li');
+                const link = document.createElement('a');
+                link.href = `${folder}/${file.name}`;
+                link.textContent = file.name.replace(/%20/g, ' ');
+                listItem.appendChild(link);
+                list.appendChild(listItem);
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching files:', error);
+    }
 }
 
 window.onload = function() {
+    console.log("window.onload executado!");
     fetchFiles('Fisiologia/Fisiologia%20I', 'fisiologiaIList');
     fetchFiles('Fisiologia/Fisiologia%20II', 'fisiologiaIIList');
     fetchFiles('Anatomia/Anatomia%20I', 'anatomiaIList');
