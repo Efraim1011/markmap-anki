256744270491f312d9c33bbfa47d722df5a719c4
EFRAIM ALMEIDA <efraimalmeida100@gmail.com>
2024-07-23 10:10:20 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
 function toggleVisibility(id) {
     var element = document.getElementById(id);
-    if (element.style.display === "none" || element.style.display === "") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
-function toggleVisibility(id) {
-    var element = document.getElementById(id);
     if (element.style.display === "none") {
         element.style.display = "block";
     } else {
