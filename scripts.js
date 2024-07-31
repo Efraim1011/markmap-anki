377b62c9e6d1dcfa6f2a2c513fac0c0489453c76
EFRAIM ALMEIDA <efraimalmeida100@gmail.com>
2024-07-30 21:45:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,36 +11,32 @@ function toggleVisibility(id) {
 
 async function fetchFiles(folder, elementId) {
     try {
-        // Construa o URL da API do GitHub
         const apiUrl = `https://api.github.com/repos/Efraim1011/markmap-anki/contents/${folder}`;
         console.log(`Buscando arquivos em: ${apiUrl}`);
         
-        // Fetch os dados do repositório GitHub
         const response = await fetch(apiUrl, {
             headers: {
                 'Accept': 'application/vnd.github.v3+json'
             }
         });
 
-        // Verificação de resposta
         if (!response.ok) {
             throw new Error(`Erro ao buscar arquivos do GitHub: ${response.statusText}`);
         }
 
-        // Parse dos arquivos retornados
         const files = await response.json();
         console.log(`Arquivos encontrados em ${folder}:`, files);
-        const list = document.getElementById(elementId);
-        list.innerHTML = ''; // Limpa os itens da lista existentes
 
-        // Loop para adicionar links na lista
+        const list = document.getElementById(elementId);
+        list.innerHTML = '';
+
         files.forEach(file => {
             if (file.type === "file" && file.name.endsWith('.html')) {
                 const listItem = document.createElement('li');
                 const link = document.createElement('a');
-                link.href = file.html_url;
-                link.textContent = decodeURIComponent(file.name.replace(/%20/g, ' '));
-                link.target = '_blank'; // Abre o link em uma nova aba
+                link.href = `https://efraim1011.github.io/markmap-anki/${folder}/${file.name}`;
+                link.textContent = decodeURIComponent(file.name.replace(/%20/g, ' ').replace('.html', ''));
+                link.target = '_blank';
                 listItem.appendChild(link);
                 list.appendChild(listItem);
             }
@@ -52,19 +48,19 @@ async function fetchFiles(folder, elementId) {
 
 window.onload = function() {
     console.log("window.onload executado!");
-    fetchFiles('1%C2%BA%20Per%C3%ADodo/Anatomia%20I', 'anatomiaIList');
-    fetchFiles('1%C2%BA%20Per%C3%ADodo/Fisiologia%20I', 'fisiologiaIList');
-    fetchFiles('2%C2%BA%20Per%C3%ADodo/Anatomia%20II', 'anatomiaIIList');
-    fetchFiles('2%C2%BA%20Per%C3%ADodo/Fisiologia%20II', 'fisiologiaIIList');
-    fetchFiles('3%C2%BAPer%C3%ADodo/Semiologia', 'semiologiaList');
-    fetchFiles('3%C2%BAPer%C3%ADodo/Patologia', 'patologiaList');
-    fetchFiles('3%C2%BAPer%C3%ADodo/Parasitologia', 'parasitologiaList');
-    fetchFiles('3%C2%BAPer%C3%ADodo/Imunologia', 'imunologiaList');
-    fetchFiles('3%C2%BAPer%C3%ADodo/Microbiologia', 'microbiologiaList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/Farmacologia', 'farmacologiaList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/Epidemiologia', 'epidemiologiaList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/Otorrinolaringologia', 'otorrinolaringologiaList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/Oftalmologia', 'oftalmologiaList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/PAPM%20IV', 'papmivList');
-    fetchFiles('4%C2%BA%20Per%C3%ADodo/Sa%C3%BAde%20da%20Fam%C3%ADlia%20IV', 'saudefamiliaivList');
+    fetchFiles('1º Período/Anatomia I', 'anatomiaIList');
+    fetchFiles('1º Período/Fisiologia I', 'fisiologiaIList');
+    fetchFiles('2º Período/Anatomia II', 'anatomiaIIList');
+    fetchFiles('2º Período/Fisiologia II', 'fisiologiaIIList');
+    fetchFiles('3ºPeríodo/Semiologia', 'semiologiaList');
+    fetchFiles('3ºPeríodo/Patologia', 'patologiaList');
+    fetchFiles('3ºPeríodo/Parasitologia', 'parasitologiaList');
+    fetchFiles('3ºPeríodo/Imunologia', 'imunologiaList');
+    fetchFiles('3ºPeríodo/Microbiologia', 'microbiologiaList');
+    fetchFiles('4º Período/Farmacologia', 'farmacologiaList');
+    fetchFiles('4º Período/Epidemiologia', 'epidemiologiaList');
+    fetchFiles('4º Período/Otorrinolaringologia', 'otorrinolaringologiaList');
+    fetchFiles('4º Período/Oftalmologia', 'oftalmologiaList');
+    fetchFiles('4º Período/PAPM IV', 'papmivList');
+    fetchFiles('4º Período/Saúde da Família IV', 'saudefamiliaivList');
 }
